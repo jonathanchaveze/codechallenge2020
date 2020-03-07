01bca7a0b4b4f7ea276f9a993a2090cf62532e2a
Jonathan Chavez <jonathan.chavez@outlook.com>
2020-03-07 18:26:14 -0500

    Implemented chat persistance service

--- WebApp/Jobsity.CodeChallenge.WebApp/Services/ChatPersistance.cs
+++ WebApp/Jobsity.CodeChallenge.WebApp/Services/ChatPersistance.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Jobsity.CodeChallenge.WebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jobsity.CodeChallenge.WebApp.Services
 {
-    public class ChatPersistance
+    public class ChatPersistance : IdentityDbContext
     {
+        public DbSet<ChatUser> ChatUsers { get; set; }
+        public DbSet<ChatPost> ChatPosts { get; set; }
+
+        public ChatPersistance(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
