82ed1427c623728669dac213f20aa1def459be70
Jonathan Chavez <jonathan.chavez@outlook.com>
2020-03-07 18:11:40 -0500

    Created initial structure

---
+++ Testing/Jobsity.CodeChallenge.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobsity.CodeChallenge.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
